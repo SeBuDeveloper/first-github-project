685521e67fa7b05ef1126f473549750bbc2e715b
Sebastian B <s.buschdorf@gmx.de>
2020-05-11 10:25:04 +0200

    Add Console-App-Project

---
+++ FirstGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
